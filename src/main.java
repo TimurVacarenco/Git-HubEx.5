0a58670dbbce82fbc208c0b797968c4f4520de4e
Tvacarenco <519043T_mama_>
2020-10-29 14:49:09 +0200

    Changes to main

--- src/main.java
+++ src/main.java
@@ -2,6 +2,9 @@ import java.sql.ClientInfoStatus;
 
 public class main {
     public static void main (String[] args)
+    {
+        System.out.println("Hello");
+    }
     public static void Client()
     {
         String name;
