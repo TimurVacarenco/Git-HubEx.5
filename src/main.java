8a3ec013258b7bf18f90f80c05743a6b254c22c4
Tvacarenco <519043T_mama_>
2020-10-29 15:14:30 +0200

    alright

--- src/main.java
+++ src/main.java
@@ -28,17 +28,4 @@ public class main {
         name="Alex";
         System.out.println(id+name);
     }
-=======
-   public static void Reg()
-   {
-       int id;
-       String name;
-   }
-   public static void GetReg(int id, String name)
-   {
-           id=1;
-           name="Alex";
-           System.out.println(id+name);
-   }
->>>>>>> 151740e7bf874accbc502a7b90271d656ebd9dc8
 }
