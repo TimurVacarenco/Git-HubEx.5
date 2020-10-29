43533288fb9e19bbdba7672cb21fb5b1e09d5507
Tvacarenco <519043T_mama_>
2020-10-29 14:47:01 +0200

    Changes to branch1

--- src/main.java
+++ src/main.java
@@ -2,9 +2,6 @@ import java.sql.ClientInfoStatus;
 
 public class main {
     public static void main (String[] args)
-    {
-        System.out.println("Hello");
-    }
     public static void Client()
     {
         String name;
