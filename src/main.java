5174f3f90e078322a9f67673a600422e5ead8530
Tvacarenco <519043T_mama_>
2020-10-29 14:34:54 +0200

    changed mainclass

--- src/main.java
+++ src/main.java
@@ -1,6 +1,19 @@
+import java.sql.ClientInfoStatus;
+
 public class main {
     public static void main (String[] args)
     {
         System.out.println("Hello");
+    }
+    public static void Client()
+    {
+        String name;
+        int age;
+    }
+    public static void GetData(String name, int age)
+    {
+        name="Alex";
+        age=21;
+        System.out.println(name+age);
     }
 }
